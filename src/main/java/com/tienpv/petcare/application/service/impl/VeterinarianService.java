80a01a140985d2b73ff531c47690fda8c6ac8f55
unknown <tienphamvan2005@gmail.com>
2024-08-13 23:23:07 +0700

    fix

--- src/main/java/com/tienpv/petcare/application/service/impl/VeterinarianService.java
+++ src/main/java/com/tienpv/petcare/application/service/impl/VeterinarianService.java
@@ -1,13 +1,15 @@
 package com.tienpv.petcare.application.service.impl;
 
+import com.tienpv.petcare.application.dto.request.VeterinarianRequest;
 import com.tienpv.petcare.application.dto.response.VeterinarianResponse;
-import com.tienpv.petcare.domain.entity.VaccinationEntity;
+import com.tienpv.petcare.application.service.validation.VeterinarinarianValidationService;
 import com.tienpv.petcare.domain.entity.VeterinarianEnity;
 import com.tienpv.petcare.domain.repository.IVeterinarianRepository;
 import com.tienpv.petcare.domain.service.IVeterinarianService;
 import com.tienpv.petcare.infrastructure.converter.IVeterinarianConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,6 +23,9 @@ public class VeterinarianService implements IVeterinarianService {
 
     @Autowired
     private IVeterinarianConverter veterinarianConverter;
+
+    @Autowired
+    private VeterinarinarianValidationService validationService;
     @Override
     public List<VeterinarianResponse> findAll(Pageable pageable) {
         List<VeterinarianResponse> results = new ArrayList<>();
@@ -35,5 +40,16 @@ public class VeterinarianService implements IVeterinarianService {
     @Override
     public int totalItem() {
         return (int) veterinarianRepository.count();
+    }
+
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
+    public VeterinarianResponse create(VeterinarianRequest request) {
+        validationService.validateVeterinarinarianRequest(request);
+        VeterinarianEnity enity = new VeterinarianEnity();
+        enity = veterinarianConverter.toEntity(request);
+        enity = veterinarianRepository.save(enity);
+        enity.setId(enity.getId());
+        return veterinarianConverter.toDTO(enity);
     }
 }
