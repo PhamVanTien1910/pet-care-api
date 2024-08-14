58364210beaf509a13da17469f058a245589aab1
Phạm Tiến <84020540+PhamVanTien1910@users.noreply.github.com>
2024-08-14 21:15:20 +0700

    render (#3)

--- src/main/java/com/tienpv/petcare/domain/constant/PublicEnpoints.java
+++ src/main/java/com/tienpv/petcare/domain/constant/PublicEnpoints.java
@@ -3,14 +3,16 @@ package com.tienpv.petcare.domain.constant;
 public class PublicEnpoints {
 
     public static final String[] POST_ENDPOINTS = {
-            "/auth/login", "/auth/introspect", "/auth/logout", "/auth/refresh", "/auth/register"
+            "/auth/login", "/auth/introspect", "/auth/logout", "/auth/refresh", "/auth/register",
+            "https://pet-care-api-3r68.onrender.com/",
+            "pet-care-api-3r68.onrender.com/",
+            "pet-care-api-3r68.onrender.com",
+            "https://pet-care-api-3r68.onrender.com",
+            "/pet-care-api/**"
     };
 
     public static final String[] GET_ENDPOINTS = {
-            "/api-docs/**", "/swagger-ui/**", "https://pet-care-api-3r68.onrender.com/",
-            "pet-care-api-3r68.onrender.com/",
-            "pet-care-api-3r68.onrender.com",
-            "/pet-care-api/**"
+            "/api-docs/**", "/swagger-ui/**", "/**", "https://**", "/"
     };
 
     private PublicEnpoints() {
