87aa505fcf4b17172549380ca4088d721e81fd05
unknown <tienphamvan2005@gmail.com>
2024-08-16 20:35:58 +0700

    PC-09: Get appoinments

--- src/main/java/com/tienpv/petcare/application/service/impl/AppointmentService.java
+++ src/main/java/com/tienpv/petcare/application/service/impl/AppointmentService.java
@@ -2,6 +2,7 @@ package com.tienpv.petcare.application.service.impl;
 
 import com.tienpv.petcare.application.dto.request.AppointmentRequest;
 import com.tienpv.petcare.application.dto.response.AppointmentResponse;
+import com.tienpv.petcare.application.dto.response.VeterinarianResponse;
 import com.tienpv.petcare.application.exception.AppException;
 import com.tienpv.petcare.application.exception.ErrorCode;
 import com.tienpv.petcare.application.service.validation.AppointmentValidationService;
@@ -15,6 +16,10 @@ import com.tienpv.petcare.domain.service.IAppointmentService;
 import com.tienpv.petcare.infrastructure.converter.IAppointmentConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AppointmentService implements IAppointmentService {
@@ -49,14 +54,45 @@ public class AppointmentService implements IAppointmentService {
             if (veterinarianEnity == null){
                 throw new AppException(ErrorCode.ID_NOT_EXISTED);
             }
-            entity.setPets(petEntity);
-            entity.setVeterinarians(veterinarianEnity);
-            entity = appointmentRepository.save(entity);
+
         }
+        if (request.getVeterinarianId() != null && request.getAppointmentDate() != null) {
+            Optional<AppointmentEntity> existingAppointment = appointmentRepository.findByVeterinarians_IdAndAppointmentDate(
+                    request.getVeterinarianId(),
+                    request.getAppointmentDate()
+            );
+
+            if (existingAppointment.isPresent()) {
+                throw new AppException(ErrorCode.PASSWORD_INVALID);
+            }
+        }
+        entity.setPets(petEntity);
+        entity.setVeterinarians(veterinarianEnity);
+        entity = appointmentRepository.save(entity);
         AppointmentResponse response;
         response = appointmentConverter.toDTO(entity);
         response.setPetName(petEntity.getName());
         response.setVeterinarianName(veterinarianEnity.getName());
         return response;
+    }
+
+    @Override
+    public List<AppointmentResponse> getList(Long request) {
+        List<AppointmentResponse> results = new ArrayList<>();
+        if(request != null){
+            List<AppointmentEntity> entities = appointmentRepository.findByPets_Id(request);
+            if(entities == null){
+                throw new AppException(ErrorCode.ID_NOT_EXISTED);
+            }
+            for (AppointmentEntity item : entities){
+                AppointmentResponse response = appointmentConverter.toDTO(item);
+                response.setPetName(item.getPets().getName());
+                response.setVeterinarianName(item.getVeterinarians().getName());
+                results.add(response);
+            }
+        }else {
+            throw new AppException(ErrorCode.INFO_INVALID);
+        }
+        return results;
     }
 }
--- src/main/java/com/tienpv/petcare/domain/repository/IAppointmentRepository.java
+++ src/main/java/com/tienpv/petcare/domain/repository/IAppointmentRepository.java
@@ -1,7 +1,14 @@
 package com.tienpv.petcare.domain.repository;
 
 import com.tienpv.petcare.domain.entity.AppointmentEntity;
+import com.tienpv.petcare.domain.entity.VeterinarianEnity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IAppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
+    Optional<AppointmentEntity> findByVeterinarians_IdAndAppointmentDate(Long veterinarianId, String appointmentDate);
+    List<AppointmentEntity> findByPets_Id(Long petId);
+
 }
